3e9a318edd728d475d95dc09a27079db734caa01
Liwanag <j4ypeer4bie@gmail.com>
2025-04-20 17:49:26 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,23 +11,23 @@ tailwind.config = {
             },
             animation:{
                 spin_slow:'spin 6s linear infinite'
-     
+            },
+            keyframes: {
+                spin: {
+                    'from': { transform: 'rotate(0deg)' },
+                    'to': { transform: 'rotate(360deg)' }
+                }
             },
             colors:{
                 lightHover: '#fcf4ff',
                 darkHover: '#2a004a',
                 darkTheme: '#11001F'
-     
             },
             boxShadow:{
                 'black': '4px 4px 0 #000',
                 'White': '4px 4px 0 #fff',
-
             }
         }
-       
-
-       
     },
     darkMode:'selector'
 }
